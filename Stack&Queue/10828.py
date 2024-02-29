ee16d4e4b35dcd9259b1d5a24f53996a6196849e
이아현 <qlqlql8448@naver.com>
2024-02-29 22:27:39 +0900

    10828 시간초과

---
+++ Stack&Queue/10828.py
@@ -0,0 +1,25 @@
+N = int(input())
+stack = []
+for i in range(N) :
+    command = input()
+    if command.startswith("push"):
+        a = command.split()
+        stack.append(a[1])
+    elif command == "pop" : 
+        if stack :
+            print(stack[-1])
+            stack.pop()
+        else : 
+            print(-1)
+    elif command == "top" :
+        if stack :
+            print(stack[-1])
+        else : 
+            print(-1)
+    elif command == "size" : 
+        print(len(stack))
+    elif command == "empty" :
+        if stack :
+            print(0)
+        else :
+            print(1)
--- Stack&Queue/a_truck_through_a_bridge.py
+++ Stack&Queue/a_truck_through_a_bridge.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def solution(bridge_length, weight, truck_weights):
     stack = []
     for t in truck_weights :
@@ -9,3 +10,15 @@ def solution(bridge_length, weight, truck_weights):
     return answer
 
 solution(2,100,[7,4,5,6])
+=======
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    stack = deque()    
+    answer = 0
+    while len(truck_weights):
+        stack.append()
+    return answer
+
+solution(2,10,[7,4,5,6])
+>>>>>>> cc25163 (10828 시간초과)
