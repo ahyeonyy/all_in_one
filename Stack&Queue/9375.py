6aefd04716d46ff4e8978418317c293b08850afd
이아현 <qlqlql8448@naver.com>
2024-03-13 21:03:31 +0900

    9375 해결

--- Stack&Queue/9375.py
+++ Stack&Queue/9375.py
@@ -1,7 +1,16 @@
-from sys import stdin
-N = int(stdin.readline())
+
+N = int(input())
 for _ in range(N):
-    n = int(stdin.readline())
+    n = int(input())
+    clothes = {}
     for _ in range(n):
-        name, types  = map(str,stdin.readline().strip().split())
-#  이름과 종류 나눠서 저장 
+        v, k  = input().split()
+        if clothes.get(k) == None:
+            clothes[k] = set()
+        clothes[k].add(v)
+    
+    count = 1
+    for key in clothes.keys():
+        count *= len(clothes[key]) + 1
+    print(count-1)
+
