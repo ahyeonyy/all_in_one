51fd1e35c74c8c7874c283166b66846926b613ec
이아현 <qlqlql8448@naver.com>
2024-03-26 17:55:39 +0900

    2941_구현중

--- 구현/2941.py
+++ 구현/2941.py
@@ -1,8 +1,10 @@
+import re
 croatia = ['c=','c-','dz=','d-','lj','nj','s=','z=']
 word = input()
 n = 0
 for c in croatia:
     if c in word:
-        word=word.replace(c,"")
+        print(word,",",c)
+        word = re.sub()
         n+=1
 print(n+len(word))
